a2f312396f772103820988964e25d47d2c452fea
leoni romain <leoniromain@hotmail.com>
2023-06-20 19:54:48 -0300

    add user add request

--- src/main/java/com/lerobeek/checkout/services/UserService.java
+++ src/main/java/com/lerobeek/checkout/services/UserService.java
@@ -22,4 +22,8 @@ public class UserService {
        Optional<User> user = userRepository.findById(id);
         return user.orElse(null);
     }
+
+    public User insert(User user){
+        return userRepository.save(user);
+    }
 }
