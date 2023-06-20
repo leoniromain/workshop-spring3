a87b5e3768b28f51c0a3f0333d9d642b1e9e64a4
leoni romain <leoniromain@hotmail.com>
2023-06-19 21:45:15 -0300

    add service and findAll and findByid

--- src/main/java/com/lerobeek/checkout/resources/userResource.java
+++ src/main/java/com/lerobeek/checkout/resources/userResource.java
@@ -1,17 +1,31 @@
 package com.lerobeek.checkout.resources;
 
 import com.lerobeek.checkout.entities.User;
+import com.lerobeek.checkout.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class userResource {
+
+    @Autowired
+    private UserService userService;
         @GetMapping
-        public ResponseEntity<User> findAll(){
-            User u = new User(1L,"leoni","leoni@gmail.com", "123456789", "leoni123");
-            return ResponseEntity.ok().body(u);
+        public ResponseEntity<List<User>> findAll(){
+            List<User> list = userService.findAll();
+            return ResponseEntity.ok().body(list);
+        }
+
+        @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+            User obj = userService.findById(id);
+            return ResponseEntity.ok().body(obj);
         }
 }
